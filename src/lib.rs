521d733f52281fdeb2fd432b44de87617bcfe70f
Eray Erdin <eraygezer.94@gmail.com>
2022-10-14 13:23:55 +0300

    add actix hello world

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+// Copyright 2022 Eray Erdin.
+// Use of this source code is governed by the WTFPL
+// license that can be found in the LICENSE file.
+
+#[macro_use]
+extern crate actix_web;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+// Copyright 2022 Eray Erdin.
+// Use of this source code is governed by the WTFPL
+// license that can be found in the LICENSE file.
+
+use std::io;
+
+use actix_web::{App, HttpServer, Responder};
+
+#[actix_web::get("/")]
+async fn hello() -> impl Responder {
+    "Hello, world!"
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
